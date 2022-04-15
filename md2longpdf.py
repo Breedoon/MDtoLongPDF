d84c7b15f8d7b8db3bfc75ce91be6693cce31ee4
Breedoon <breedoon@programmer.net>
2022-04-15 22:21:12 +0100

    Added option to run from command line
    If command line arguments are empty, will prompt the user to enter the argument

--- md2longpdf.py
+++ md2longpdf.py
@@ -1,4 +1,5 @@
 from modules import *
+import argparse
 
 
 def to_pdf(in_filepath, out_filepath_pdf):
@@ -26,7 +27,7 @@ def _preprocess_inputs(in_file, out_path):
     # Parse out location of md file and its name
     dir_path, filename, _ = _get_path_name_ext(in_file)
 
-    if len(out_path.strip()) == 0:  # output path is not given, put it next to the input file
+    if out_path is None or len(out_path.strip()) == 0:  # output path is not given, put it next to the input file
         out_path = dir_path
     else:
         out_path = os.path.realpath(out_path.strip()) + '/'  # ensure has / at the end
@@ -74,9 +75,28 @@ def main(in_file, out_path):
     to_pdf(in_file, out_pdf)
 
 
-if __name__ == '__main__':
+def _get_args_from_input():
     in_file = input('Enter location of your md/html/ipynb file (e.g.: /Users/breedoon/CS166/Assignment 1.md)\n')
     out_path = input('Enter location to store the produced PDF file (e.g.: /Users/breedoon/CS166/PDF/)\n'
                      'or leave blank to generate in the same folder\n')
+
+    return in_file, out_path
+
+
+def _get_args_from_command():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--input-file', '-i', dest='in_file', required=False,
+                        help='absolute path to the md/html/ipynb file')
+    parser.add_argument('--output-path', '-o', dest='out_path', required=False,
+                        help='absolute path to directory where to put the produced PDF file')
+    args = parser.parse_args()
+    return args.in_file, args.out_path
+
+
+if __name__ == '__main__':
+    in_file, out_path = _get_args_from_command()
+
+    if in_file is None:  # args weren't passed via command line, ask to input
+        in_file, out_path = _get_args_from_input()
 
     main(in_file, out_path)
